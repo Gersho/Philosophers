e2da46fddf2b1bd4c539bbaafa36d7690c090233
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-05-11 16:20:38 +0200

    nothing much

---
+++ philo_one/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/11 16:05:15 by kzennoun          #+#    #+#             */
+/*   Updated: 2021/05/11 16:20:04 by kzennoun         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+philosopher with threads and mutex
+
+	memset,
+	printf,
+	malloc, 
+	free, 
+	write,
+
+	usleep,
+	gettimeofday
+
+	pthread_create,
+	pthread_detach,
+	pthread_join,
+	
+	pthread_mutex_init,
+	pthread_mutex_destroy,
+	pthread_mutex_lock,
+	pthread_mutex_unlock
+
+	   */
---
+++ philo_three/main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/11 16:07:26 by kzennoun          #+#    #+#             */
+/*   Updated: 2021/05/11 16:19:27 by kzennoun         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+
+philosopher with processes and semaphore
+
+	memset,
+	printf,
+	malloc, 
+	free, 
+	write,
+
+	fork,
+	kill,
+	exit,
+
+	pthread_create,
+	pthread_detach,
+	pthread_join,
+
+	usleep,
+	gettimeofday,
+
+	waitpid, 
+	
+	sem_open,
+	sem_close,
+	sem_post,
+	sem_wait,
+	sem_unlink 
+
+			*/
